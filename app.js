896a135ce35ee5b7508456289fe0756924064bf6
viren7801 <virenpatel1912@gmail.com>
2023-04-04 21:32:16 +0530

    added files

---
+++ app.js
@@ -0,0 +1,31 @@
+var btnTranslate = document.querySelector("#btn-translate");
+var txtInput = document.querySelector("#text-input");
+var outputDiv = document.querySelector("#output");
+
+var serverURL = "https://api.funtranslations.com/translate/minion.json"
+
+function getTranslationURL(text) {
+    return serverURL + "?" + "text=" + text
+}
+
+// function errorHandler(error) {
+//     console.log("error occured", error);
+//     alert("Something went wrong! try again later")
+// }
+
+function clickEventHandler() {
+    var inputText = txtInput.value;
+
+    fetch(getTranslationURL(inputText))
+    .then(response => response.json())
+    .then(json => {
+        var translatedText = json.contents.translated;
+        outputDiv.innerText = translatedText;
+    })
+    // .catch(errorHandler)
+}
+
+
+
+btnTranslate.addEventListener("click", clickEventHandler)
+
